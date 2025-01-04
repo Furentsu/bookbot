997e2131b69d4e337574fd5506737ccaaafbf336
Eugenio Franzoso <furentsu@EUGENIOS-MBP.station>
2025-01-04 10:13:01 +0100

    added a function to count the total number of characters' occurrencies

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import json
+
 def main():
 
     path = "/Users/furentsu/programming/github.com/Furentsu/bookbot/books/frankenstein.txt"
     
     with open(path) as f:
         file_contents =f.read()
-        print(count_words(file_contents))
+        print("Total characters in the book: \n", json.dumps(count_characters_occurrencies(file_contents), indent=4, sort_keys=True))
+        print("\nTotal words: ", count_words(file_contents))
 
 # Function to count the number of words in the text
 def count_words(text):
     words = text.split()
     return len(words)
+
+# Function to count the number of characters occurrencies in the text
+def count_characters_occurrencies(text):
+    characters = {}
+    for character in text.lower():
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    return characters
 
 main()
